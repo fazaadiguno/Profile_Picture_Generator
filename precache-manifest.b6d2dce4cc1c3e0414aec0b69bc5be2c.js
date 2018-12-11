6706ff13a26a74a092055695e2c9c4efa0f1893a
fazaadiguno <faza.adiguno@gmail.com>
2018-12-11 15:26:09 +0700

    Add files via upload

---
+++ precache-manifest.b6d2dce4cc1c3e0414aec0b69bc5be2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Profile_Picture_Generator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "058ff090ebb672488392",
+    "url": "/Profile_Picture_Generator/static/js/runtime~main.058ff090.js"
+  },
+  {
+    "revision": "57b2187aab25262eea78",
+    "url": "/Profile_Picture_Generator/static/js/main.57b2187a.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/Profile_Picture_Generator/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "57b2187aab25262eea78",
+    "url": "/Profile_Picture_Generator/static/css/main.af1c34a6.chunk.css"
+  },
+  {
+    "revision": "e8172ec49e027315b1110e455e7a8bd3",
+    "url": "/Profile_Picture_Generator/index.html"
+  }
+];
